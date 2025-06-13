0e0acbfc35bf912bd8d1d783847671c67f084217
Johannes2240 <loajohanesse1@gmail.com>
2025-06-13 10:11:49 -0700

    Create Number of Provinces.cpp

---
+++ DSA fold/Number of Provinces.cpp
@@ -0,0 +1,45 @@
+// Graph Type: Undirected Graph represented as an Adjacency List
+class Solution {
+public:
+    void buildGraph(const vector<vector<int>>& isConnected, vector<vector<int>>& adjList) {
+        int n = isConnected.size();
+        for (int i = 0; i < n; ++i) {
+            for (int j = i; j < n; ++j) {
+                if (isConnected[i][j] == 1) {
+                    adjList[i].push_back(j);
+                    if (i != j) {
+                        adjList[j].push_back(i); 
+                    }
+                }
+            }
+        }
+    }
+
+
+    void dfs(int city, vector<bool>& visited, const vector<vector<int>>& adjList) {
+        visited[city] = true;
+        for (int neighbor : adjList[city]) {
+            if (!visited[neighbor]) {
+                dfs(neighbor, visited, adjList);
+            }
+        }
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<vector<int>> adjList(n);
+        vector<bool> visited(n, false);
+
+        buildGraph(isConnected, adjList); 
+
+        int provinceCount = 0;
+        for (int i = 0; i < n; ++i) {
+            if (!visited[i]) {
+                dfs(i, visited, adjList);
+                provinceCount++; 
+            }
+        }
+
+        return provinceCount;
+    }
+};
