eba4395c2847b6a9d5468330c1ad191c4d73a5ba
Johannes2240 <loajohanesse1@gmail.com>
2025-06-13 10:17:20 -0700

    Create Sum of Nodes with Even-Valued Grandparent.cpp

---
+++ DSA fold/Sum of Nodes with Even-Valued Grandparent.cpp
@@ -0,0 +1,20 @@
+// Graph used: Binary Tree (a rooted, directed, acyclic graph)
+
+class Solution {
+public:
+    void dfs(TreeNode* node, int parentVal, int grandparentVal, int& total) {
+        if (!node) return;
+
+        if (grandparentVal % 2 == 0)
+            total += node->val;
+
+        dfs(node->left, node->val, parentVal, total);
+        dfs(node->right, node->val, parentVal, total);
+    }
+
+    int sumEvenGrandparent(TreeNode* root) {
+        int sum = 0;
+        dfs(root, -1, -1, sum);
+        return sum;
+    }
+};
